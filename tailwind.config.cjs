911d5e4c2adf75d9124dc45a9748ed0fbfdbc93e
Florian König <koenidv@gmail.com>
2023-05-29 21:40:33 +0200

    Proper projects content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins", ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
-	plugins: [],
-}
+	plugins: []
+};
