4e694b48678653b32e8eddb89a60b839962283c2
Florian König <koenidv@gmail.com>
2023-06-11 12:49:57 +0200

    Extract dialog logic

---
+++ src/scripts/dialogbuttons.js
@@ -0,0 +1,55 @@
+const showDialog = (origin, dialog) => {
+	origin.style.viewTransitionName = "";
+	dialog.showModal();
+	origin.classList.add("invisible");
+	document.body.style.overflowY = "hidden";
+	document.body.style.top = `-${scroll}px`;
+	// Disable scrolling: https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
+};
+
+document.querySelectorAll(".opendialog").forEach((e) => {
+	const origin = document.getElementById(e.getAttribute("data-origin"));
+	const dialog = document.getElementById(e.getAttribute("data-dialog"));
+
+	e.addEventListener("click", () => {
+		origin.style.viewTransitionName = "fullembed";
+		const scroll = window.scrollY;
+		if (!document.startViewTransition) {
+			showDialog(origin, dialog);
+			return;
+		}
+
+		document.startViewTransition(() => {
+			showDialog(origin, dialog);
+		});
+	});
+});
+
+const hideDialog = (origin, dialog) => {
+	origin.style.viewTransitionName = "fullembed";
+	dialog.close();
+	origin.classList.remove("invisible");
+	document.body.style.overflowY = "";
+	document.body.style.top = "";
+};
+
+const handleCloseClicked = async (origin, dialog) => {
+	if (!document.startViewTransition) {
+		hideDialog(origin, dialog);
+		return;
+	}
+
+	const transaction = document.startViewTransition(() => {
+		hideDialog(origin, dialog);
+	});
+	await transaction.updateCallbackDone;
+	origin.style.viewTransitionName = "";
+};
+
+document.querySelectorAll(".closedialog").forEach((e) => {
+	const origin = document.getElementById(e.getAttribute("data-origin"));
+	const dialog = document.getElementById(e.getAttribute("data-dialog"));
+
+	e.addEventListener("mousedown", () => handleCloseClicked(origin, dialog));
+	e.addEventListener("touchstart", () => handleCloseClicked(origin, dialog));
+});
