679dbb9ad43823892f0917a836e9d8ec615ca1b6
Florian König <koenidv@gmail.com>
2024-06-03 20:12:09 +0100

    chore: new background color and grain

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,22 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				poppins: ["Poppins", ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				beige: {
+					50: "#fcfbf7",
+					100: "#f5f0e5",
+					200: "#eee5d3",
+					300: "#e7dbc0",
+					400: "#e0d0ae",
+				},
+				mojito: {
+					50: "#fbfff5",
+					100: "#f1ffdc",
+					200: "#e7ffc2",
+				}
 			}
-		}
+		},
 	},
 	plugins: []
 };
