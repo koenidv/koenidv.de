2b2dc847f60071eed229db42de73af6b5d001d62
Florian König <koenidv@gmail.com>
2024-09-10 12:26:34 +0200

    use hybrid output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from "astro/config";
 import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import { domains } from "googleapis/build/src/apis/domains";
 
 // https://astro.build/config
 export default defineConfig({
-	// output: "server",
+	output: "hybrid",
 	site: "https://koeni.dev",
 	compressHTML: true,
 	publicDir: "public",
 	srcDir: "src",
-	// adapter: netlify(),
+	adapter: netlify(),
 	integrations: [
 		tailwind(),
 		mdx()
