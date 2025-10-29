47cf8a3ff63ee9097edcab4545640c28bed803f7
Florian König <32238636+koenidv@users.noreply.github.com>
2025-10-29 19:32:12 +0100

    feat: parameterize promoted content

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,12 @@ import { defineCollection, z } from "astro:content";
 const projects = defineCollection({
   schema: z.object({
     title: z.string(),
+    alternativeTitle: z.string().optional(),
+    shortTitle: z.string().optional(),
     description: z.string(),
     longDescription: z.string().optional(),
     image: z.string().optional(),
+    video: z.string().optional(),
     link: z.string(),
     tags: z.array(z.string()),
     date: z.string().or(z.date()).transform((val) => new Date(val)),
@@ -14,6 +17,7 @@ const projects = defineCollection({
     live: z.boolean().optional(),
     ctaLinkText: z.string().optional(),
     draft: z.boolean().optional(),
+    type: z.enum(["project", "article"]).optional(),
   }),
 });
 
